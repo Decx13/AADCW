34c7323f21bd8f7642a2be728ff27d5f3e2453f1
amandimaheshavi <amandimaheshavi@gmail.com>
2023-10-19 13:46:05 +0530

    adding

--- User Micro Service/src/main/java/lk/ijse/gdse63/travelcw/dto/UserDTO.java
+++ User Micro Service/src/main/java/lk/ijse/gdse63/travelcw/dto/UserDTO.java
@@ -1,22 +1,38 @@
 package lk.ijse.gdse63.travelcw.dto;
-    import lombok.AllArgsConstructor;
+
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 
-    @Data
-    @AllArgsConstructor
-    @NoArgsConstructor
-    public class UserDTO {
-        private int id;
-        private String username;
-        private String password;
-        private String usernic;
-        private String contact;
-        private String email;
-        private LocalDate birthday;
-        private String[] nicImg;
-        private String gender;
-        private String remarks;
+@Data
+@AllArgsConstructor
+
+public class UserDTO {
+    private int id;
+    private String username;
+    private String password;
+    private String usernic;
+    private String contact;
+    private String email;
+    private LocalDate birthday;
+    private String nicFront;
+    private String nicRear;
+    private ArrayList<String> roles;
+    private String gender;
+    private String remarks;
+    private String profilePic;
+
+    private byte[] profilePicByte;
+    private byte[] nicFrontByte;
+    private byte[] nicRearByte;
+
+
+
+    public UserDTO(){
+        this.roles = new ArrayList<>();
+        roles.add("user");
     }
+}
